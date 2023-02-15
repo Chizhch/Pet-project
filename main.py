99a022663cca4a0d9e370f2daa51b7bae18e439b
Chizhch <chizhikov111@gmail.com>
2023-02-15 21:28:07 +0300

    new project 1.0

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+@app.route("/")
+def home():
+    return render_template("home.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if username == "admin" and password == "secret":
+            return redirect(url_for("secret"))
+        else:
+            return "Incorrect login. Please try again."
+    return render_template("login.html")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route("/secret")
+def secret():
+    return "You have successfully logged in!"
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app.run()
